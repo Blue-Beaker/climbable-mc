ef9a261705a524313938d79e580ce53e3c1a63f5
blue-beaker <yyy5566778899@126.com>
2023-07-10 17:50:35 +0800

    update mcmod.info and version

--- src/main/java/io/bluebeaker/climbable/ClimbableMod.java
+++ src/main/java/io/bluebeaker/climbable/ClimbableMod.java
@@ -12,7 +12,7 @@ public class ClimbableMod
 {
     public static final String MODID = "climbable";
     public static final String NAME = "Climbable";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.0";
 
     private static Logger logger;
 
